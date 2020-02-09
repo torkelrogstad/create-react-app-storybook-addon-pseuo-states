82b5ddf063d8fe4b17bea489daab71b7e0970ff2
Philipp Schardt <philipp.schardt@ergosign.de>
2020-02-09 18:41:23 +0100

    working with preset

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,16 @@
-const path = require('path');
+// const path = require('path');
 
 module.exports = {
     stories: ['../src/**/*.stories.[tj]s'],
-    presets: [
+    addons: [
         {
             name: '@storybook/preset-create-react-app',
             options: {
                 tsDocgenLoaderOptions: {},
             },
         },
+        'storybook-addon-pseudo-states-react',
         'storybook-addon-pseudo-states-react/preset-postcss',
-        path.resolve('./.storybook/preset-postcss'),
-    ],
-    addons: [
-        'storybook-addon-pseudo-states-react/register',
         '@storybook/addon-knobs',
         '@storybook/addon-actions',
         '@storybook/addon-links',
