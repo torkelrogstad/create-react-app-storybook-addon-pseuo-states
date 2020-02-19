d1ff15fff848eb42cdcad945fce0e2674acc8fc9
Philipp Schardt <philipp.schardt@ergosign.de>
2020-02-19 10:49:38 +0100

    udpate to latest version

---
+++ src/stories/2-Button.stories.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {action} from '@storybook/addon-actions';
+import {Button} from '@storybook/react/demo';
+import {withPseudo} from "storybook-addon-pseudo-states-react";
+import {SimpleButton} from "../SimpleButton/simple-button";
+// import {storiesOf} from "@storybook/react";
+
+
+export default {
+    title: 'Button',
+    decorators: [
+        withPseudo
+    ],
+    parameters: {
+        withPseudo: {
+            attributes: []
+        }
+    }
+};
+
+export const simpleButton2 = () => <SimpleButton label={'ButtonLabel'}/>;
