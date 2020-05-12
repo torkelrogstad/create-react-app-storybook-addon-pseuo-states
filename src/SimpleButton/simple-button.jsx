ef54c39864124975bd4630052327d3ed9499a9b4
Torkel Rogstad <torkel@rogstad.io>
2020-05-12 16:43:24 +0200

    attempt to avoid Scss modules

--- src/SimpleButton/simple-button.jsx
+++ src/SimpleButton/simple-button.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import style from './simple-button.module.scss';
+import './simple-button.scss';
 import classNames from 'classnames';
 
 export const SimpleButton = (props) => {
@@ -10,8 +10,8 @@ export const SimpleButton = (props) => {
             onClick={props.onClick ? props.onClick : () => null}
             disabled={props.disabled}
             className={classNames(
-                style.Button,
-                {[style.big]: props.isBig}
+                "Button",
+                {"big": props.isBig}
             )}
         >
             {props.label}
